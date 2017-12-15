e962841a3a14aa2ffa5850ae0a4d87f4658cd90a
torvalds <torvalds@linux-foundation.org>
2017-12-15 13:54:49 +0800

    add ncserv

--- ncserv.sh
+++ ncserv.sh
@@ -1,1 +1,1 @@
- while true; do { echo -e "HTTP/1.1 200 OK\r\n\r\n`date`"; bash -c test;} | nc -l 8000; done
+ while true; do { echo -e "HTTP/1.1 200 OK\r\n\r\n`date`"; bash -c test;} | nc -l 8000; done 
